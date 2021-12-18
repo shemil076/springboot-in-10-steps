9f08314f011f077abef21a8dad4354e30e2959c1
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-18 14:24:26 +0530

    Creating a REST controller

---
+++ src/main/java/com/springbootBasics/springbootin10steps/Book.java
@@ -0,0 +1,25 @@
+package com.springbootBasics.springbootin10steps;
+
+public class Book {
+    long id;
+    String name, author;
+
+    public Book(long id, String name, String author) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/main/java/com/springbootBasics/springbootin10steps/BooksController.java
@@ -0,0 +1,15 @@
+package com.springbootBasics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return Arrays.asList(new Book(1, "Master spring", "Ranga"));
+    }
+}
